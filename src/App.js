3ade3194ef811602c2ac9871419d61deddade875
k3sforall <111164043+k3sforall@users.noreply.github.com>
2025-09-09 19:41:06 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-           999999 github push jenkins 빌드
+           01010101 github push jenkins 빌드
         </a>
       </header>
     </div>
